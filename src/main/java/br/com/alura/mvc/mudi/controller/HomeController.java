bc02132a27c5582b43f8f3a04d14d3f776826fa3
Márcio Lopes <mlsmarciogm@gmail.com>
2022-12-19 23:46:55 -0300

    Adicionar a exibição dos pedidos do banco de dados, adicionando uma conexão com o banco integrado via JPA com o springboot. No start.spring.io adicionar o Spring Data JPA e o MySQL Driver, no explorer copiar as dependências e implementar no pom. Configurar o application.properties
    Para o spring criar a tabela necessária, na classe Pedido adicionar a notação @Entity, no atributo id adicionar
    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
    
    No Controller Criar um EntityManager e utilizá-lo para obter os dados do BD.

--- src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -3,6 +3,10 @@ package br.com.alura.mvc.mudi.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,16 +16,14 @@ import br.com.alura.mvc.mudi.model.Pedido;
 @Controller
 public class HomeController {
 	
+	@PersistenceContext
+	private EntityManager entityManager;
+	
 	@GetMapping("/home")
 	public String home(Model model) {
 		
-		Pedido pedido = new Pedido();
-		pedido.setNomeProduto("Smartphone Samsung Galaxy A13 ");
-		pedido.setUrlImagem("https://a-static.mlcdn.com.br/800x560/smartphone-samsung-galaxy-m13-128gb-verde-4g-octa-core-4gb-ram-66-cam-tripla-selfie-8mp/magazineluiza/235392700/21f35089ba29e513c714d74625794ce1.jpg");
-		pedido.setUrlProduto("https://www.amazon.com.br/dp/B0B61W6MVF?ref=dlx_deals_gd_dcl_img_25_609dd41f_dt_sl16_bc");
-		pedido.setDescricao("Preto 128GB 4GB RAM bateria 5000mAh Câmera Quádrupla Traseira de 50MP + 5MP + 2MP + 2MP Octa-Core tela infinita");
-		
-		List<Pedido> pedidos = Arrays.asList(pedido);
+		Query query = entityManager.createQuery("select p from Pedido p", Pedido.class);
+		List<Pedido> pedidos = query.getResultList();
 		model.addAttribute("pedidos", pedidos);
 		
 		return "home";
