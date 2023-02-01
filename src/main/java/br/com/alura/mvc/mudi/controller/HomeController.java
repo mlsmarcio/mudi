82627b198df08bc9974a0864b2db23970ac9d693
Márcio Lopes <mlsmarciogm@gmail.com>
2023-01-31 22:03:03 -0300

    Utilizando o Spring Data JPA - Torna a comunicação com bd mais simples com muitos métodos implementados. Configurando: Utiliza interface para comunicação com o bd, é necessário extender a interface JpaRepository<Pedido, Long>, adicionar a notação @Repository utiliza um padrão de nomenclatura nos métodos

--- src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -1,25 +1,22 @@
 package br.com.alura.mvc.mudi.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import br.com.alura.mvc.mudi.model.Pedido;
 import br.com.alura.mvc.mudi.repository.PedidoRepository;
 
 @Controller
 public class HomeController {
 	
 	@Autowired	// pedi para o spring a instância
-	private PedidoRepository repository;
+	private PedidoRepository pedidoRepository;
 	
 	@GetMapping("/home")
 	public String home(Model model) {
-		List<Pedido> pedidos = repository.recuperaTodosOsPedidos();
-		model.addAttribute("pedidos", pedidos);
+		//List<Pedido> pedidos = pedidoRepository.findAll();
+		model.addAttribute("pedidos", pedidoRepository.findAll());
 		return "home";
 	}
 }
