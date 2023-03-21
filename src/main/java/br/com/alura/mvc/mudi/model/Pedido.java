19b19efdc0c5e102ef0a10e520c019d05c351fc4
Márcio Lopes <mlsmarciogm@gmail.com>
2023-03-20 22:17:48 -0300

    Associano o usuário logado ao pedido

--- src/main/java/br/com/alura/mvc/mudi/model/Pedido.java
+++ src/main/java/br/com/alura/mvc/mudi/model/Pedido.java
@@ -33,6 +33,14 @@ public class Pedido {
 	@ManyToOne(fetch = FetchType.LAZY)
 	private User user;
 
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
 	public StatusPedido getStatus() {
 		return status;
 	}
