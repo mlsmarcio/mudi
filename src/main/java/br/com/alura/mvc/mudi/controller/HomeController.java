f65ca5407e1d03c9bd8ecd5828f4500e847d2aeb
Márcio Lopes <mlsmarciogm@gmail.com>
2023-03-20 21:39:14 -0300

    Mostrando os pedidos do usuário

--- src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -1,5 +1,8 @@
 package br.com.alura.mvc.mudi.controller;
 
+import java.security.Principal;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import br.com.alura.mvc.mudi.model.Pedido;
 import br.com.alura.mvc.mudi.model.StatusPedido;
 import br.com.alura.mvc.mudi.repository.PedidoRepository;
 
@@ -19,9 +23,9 @@ public class HomeController {
 	private PedidoRepository pedidoRepository;
 	
 	@GetMapping
-	public String home(Model model) {
-		//List<Pedido> pedidos = pedidoRepository.findAll();
-		model.addAttribute("pedidos", pedidoRepository.findAll());
+	public String home(Model model, Principal principal) {
+		List<Pedido> pedidos = pedidoRepository.findAllByUsuario(principal.getName());
+		model.addAttribute("pedidos", pedidos);
 		return "home";
 	}
 
